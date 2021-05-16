6a2d0236d3f49ce8a4e7e659f363eca916638c2a
uuboyscy <u.shi@lndata.com>
2021-05-16 09:35:05 +0800

    update20210515

---
+++ pyetl/cfb101-course/15_dcardPractice.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = 'https://www.dcard.tw/service/api/v2/forums/photography/posts?limit=30&before=235903762'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+}
+
+res = requests.get(url, headers=headers)
+# print(res.text)
+jsonData = json.loads(res.text) # list
+
